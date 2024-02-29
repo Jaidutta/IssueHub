edb88defea1363f37af744d03ef3fe3e56dbefdf
Joy Dutta <dutta_joy@hotmail.com>
2024-02-29 15:14:16 +0000

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using IssueHub.Models;
+
+namespace IssueHub.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<IssueHubUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
