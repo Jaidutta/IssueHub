4035956263ffc79b29defb3aaae3e3024a811651
Joy Dutta <dutta_joy@hotmail.com>
2024-03-03 23:25:07 +0000

    Adds IIssueHubCompanyInfoService Interface

---
+++ Services/Interfaces/IIssueHubCompanyInfoService.cs
@@ -0,0 +1,15 @@
+﻿using IssueHub.Models;
+
+namespace IssueHub.Services.Interfaces
+{
+    public interface IIssueHubCompanyInfoService
+    {
+       public Task<Company> GetCompanyInfoByIdAsync(int? companyId);
+
+        public Task<List<IssueHubUser>> GetAllMemberAsync(int companyId);
+
+        public Task<List<Project>> GetAllProjectsAsync(int companyId);
+
+        public Task<List <Ticket>> GetAllTicketsAsync(int companyId);
+    }
+}
