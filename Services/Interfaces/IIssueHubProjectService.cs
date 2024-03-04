54dd8466b151a5c7afabd54218aef6f8ee8c0b45
Joy Dutta <dutta_joy@hotmail.com>
2024-03-04 17:40:35 +0000

    implements CRUD operations for Project through IssueHubProjectService

---
+++ Services/Interfaces/IIssueHubProjectService.cs
@@ -0,0 +1,49 @@
+﻿using IssueHub.Models;
+
+namespace IssueHub.Services.Interfaces
+{
+    public interface IIssueHubProjectService
+    {
+        public Task AddNewProjectAsync(Project project);
+
+        public Task<bool> AddProjectManagerAsync(string userId, int projectId);
+
+        public Task<bool> AddUserToProjectAsync(string userId, int projectId);
+
+        public Task ArchiveProjectAsync(Project project);
+
+        public Task<List<Project>> GetAllProjectsByCompany(int companyId);
+
+        public Task<List<Project>> GetAllProjectsByPriority(int companyId, string priorityName);
+
+        public Task<List<IssueHubUser>> GetAllProjectMembersExceptPMAsync(int projectId);
+
+        public Task<List<Project>> GetArchivedProjectsByCompany(int companyId);
+
+        public Task<List<IssueHubUser>> GetDevelopersOnProjectAsync(int projectId);
+
+        public Task<IssueHubUser> GetProjectManagerAsync(int projectId);
+
+        public Task<List<IssueHubUser>> GetProjectMembersByRoleAsync(int projectId, string role);
+
+        public Task<Project> GetProjectByIdAsync(int projectId, int companyId);
+
+        public Task<List<IssueHubUser>> GetSubmittersOnProjectAsync(int projectId);
+
+        public Task<List<IssueHubUser>> GetUsersNotOnProjectAsync(int projectId, int companyId);
+
+        public Task<List<Project>> GetUserProjectsAsync(string userId);
+
+        public Task<bool> IsUserOnProject(string userId, int projectId);
+
+        public Task<int> LookupProjectPriorityId(string priorityName);
+
+        public Task RemoveProjectManagerAsync(int projectId);
+
+        public Task RemoveUsersFromProjectByRoleAsync(string role, int projectId);
+
+        public Task RemoveUserFromProjectAsync(string userId, int projectId);
+
+        public Task UpdateProjectAsync(Project project);
+    }
+}
---
+++ Services/IssueHubProjectService.cs
@@ -0,0 +1,143 @@
+﻿using IssueHub.Data;
+using IssueHub.Models;
+using IssueHub.Services.Interfaces;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace IssueHub.Services
+{
+    public class IssueHubProjectService : IIssueHubProjectService
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly RoleManager <IdentityRole>_roleManager;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public IssueHubProjectService(ApplicationDbContext context, RoleManager<IdentityRole> roleManager, UserManager<IdentityUser> userManager)
+        {
+            _context = context;
+            _roleManager = roleManager;
+            _userManager = userManager;
+        }
+
+        // Create Project
+        public async Task AddNewProjectAsync(Project project)
+        {
+            _context.Add(project);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<bool> AddProjectManagerAsync(string userId, int projectId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> AddUserToProjectAsync(string userId, int projectId)
+        {
+            throw new NotImplementedException();
+        }
+
+        // Archive or "Delete" Project
+        public async Task ArchiveProjectAsync(Project project)
+        {
+            project.Archived = true;
+            _context.Update(project);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<List<IssueHubUser>> GetAllProjectMembersExceptPMAsync(int projectId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Project>> GetAllProjectsByCompany(int companyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<Project>> GetAllProjectsByPriority(int companyId, string priorityName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Project>> GetArchivedProjectsByCompany(int companyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<IssueHubUser>> GetDevelopersOnProjectAsync(int projectId)
+        {
+            throw new NotImplementedException();
+        }
+
+        // Read
+        public async Task<Project> GetProjectByIdAsync(int projectId, int companyId)
+        {
+            Project project = await _context.Projects
+                                                .Include(p => p.Tickets)
+                                                .Include(p => p.ProjectPriority)
+                                                .Include(p => p.Members)
+                                                .FirstOrDefaultAsync(p => p.Id == projectId 
+                                                    && p.CompanyId == companyId);
+            return project;
+
+        }
+
+        public async Task<IssueHubUser> GetProjectManagerAsync(int projectId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<IssueHubUser>> GetProjectMembersByRoleAsync(int projectId, string role)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<IssueHubUser>> GetSubmittersOnProjectAsync(int projectId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<Project>> GetUserProjectsAsync(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<IssueHubUser>> GetUsersNotOnProjectAsync(int projectId, int companyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> IsUserOnProject(string userId, int projectId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<int> LookupProjectPriorityId(string priorityName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task RemoveProjectManagerAsync(int projectId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task RemoveUserFromProjectAsync(string userId, int projectId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task RemoveUsersFromProjectByRoleAsync(string role, int projectId)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        // Edit
+        public async Task UpdateProjectAsync(Project project)
+        {
+            _context.Update(project);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
